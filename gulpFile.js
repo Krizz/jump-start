b847cb139508951167c972dfa14a6ce544242472
unknown <kriz@live.com>
2014-11-19 23:49:13 +0200

    stylus

--- gulpFile.js
+++ gulpFile.js
@@ -1,18 +1,16 @@
 var browserify = require('browserify');
-var es6ify = require('es6ify');
 var gulp = require('gulp');
 var source = require('vinyl-source-stream');
 var gutil = require('gulp-util');
 var jade = require('gulp-jade');
 var plumber = require('gulp-plumber');
 var uglify = require('gulp-uglify');
-var less = require('gulp-less');
 var browserSync = require('browser-sync');
 var prefix = require('gulp-autoprefixer');
 var path = require('path');
+var stylus = require('gulp-stylus');
 var watchify = require('watchify');
-
-es6ify.traceurOverrides = { blockBinding: true };
+var nib = require('nib');
 
 gulp.task('compress', function() {
   gulp.src('lib/*.js')
@@ -34,8 +32,7 @@ gulp.task('scripts', function() {
   var src = './src/js/app.js';
   var dest = './dist/js/';
 
-  var bundler = watchify(browserify(es6ify.runtime, watchify.args));
-  bundler.add(src);
+  var bundler = watchify(browserify(src, watchify.args));
   bundler.on('update', rebundle);
   function rebundle() {
     return bundler.bundle()
@@ -64,9 +61,15 @@ gulp.task('jade', function () {
 });
 
 gulp.task('styles', function () {
-  gulp.src('./src/styles/style.less')
-    .pipe(less({
-      paths: [ path.join(__dirname, 'less', 'includes') ]
+  gulp.src('./src/styles/style.styl')
+    .pipe(stylus({
+      compress: true,
+      "include css": true,
+      use: nib(),
+      set:[
+        'include css',
+        'compress'
+      ]
     }))
     .on('error', gutil.log)
     .pipe(prefix())
@@ -88,7 +91,7 @@ gulp.task('browser-sync', function() {
 gulp.task('watch', function() {
   gulp.watch(['./src/js/app.js'], ['scripts']);
   gulp.watch(['src/**/*.jade'], ['jade']);
-  gulp.watch(['src/**/*.less'], ['styles']);
+  gulp.watch(['src/**/*.styl'], ['styles']);
   gulp.watch(['src/images/**', 'src/fonts/**'], ['copy-assets']);
 });
 
--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,1 @@
-
-class Car {
-  constructor(name) {
-    console.log('New car is ' + name)
-  }
-}
-
-var car = new Car('BMW');
-
-alert(car);
+var $ = require('jquery')
